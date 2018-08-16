85ce6a10d177d4014eebd29228ca3eceb2ef34cc
ziyuzhan <ziyuzhan@cisco.com>
2018-08-16 17:23:38 +0800

    gh-pages

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/vue-game-gobang/',
+    assetsPublicPath: '/vue-game-Gobang/',
 
     /**
      * Source Maps
